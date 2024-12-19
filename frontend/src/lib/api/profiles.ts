8f4e89bdfc541e5c0e26bdf957537ef732be6b5b
TheDLCrimson <duclnc2004@gmail.com>
2024-12-20 03:28:10 +0700

    [FIX] FND-97 Change URL to connect backend

--- frontend/src/lib/api/profiles.ts
+++ frontend/src/lib/api/profiles.ts
@@ -2,8 +2,8 @@ import useAuthenticatedAxios from "@/hooks/useAuthenticatedAxios";
 
 export const useProfileAPI = () => {
   const { makeAuthenticatedRequest } = useAuthenticatedAxios();
-  const BASE_URL = "https://2347bb17-7489-41d1-8035-912e50be3857.mock.pstmn.io/";
-
+  //const BASE_URL = "https://2347bb17-7489-41d1-8035-912e50be3857.mock.pstmn.io/";
+  const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
   const getUserProfiles = async () => {
     return await makeAuthenticatedRequest(`${BASE_URL}/getUserProfiles`, "GET");
   };
